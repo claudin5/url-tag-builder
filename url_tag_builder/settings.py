960684e5b97485a31223a382e641e2291acec23a
claudio <43114940+claudin5@users.noreply.github.com>
2021-01-31 16:34:22 -0500

    change whitenoise config

--- url_tag_builder/settings.py
+++ url_tag_builder/settings.py
@@ -131,4 +131,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
