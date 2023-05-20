de20626064bc6ad6d2ace001f8bfffee8a567a30
claudio <claudio@mx-mac>
2023-05-19 23:13:49 -0400

    update host

--- url_tag_builder/settings.py
+++ url_tag_builder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-MAIN_HOST = '*'
+MAIN_HOST = 'url-tag-builder.up.railway.app'
 ALLOWED_HOSTS = [MAIN_HOST]
 
 
