471259fa5713a505342230d3b0234dcd420646b2
claudio <claudio@mx-mac>
2023-05-21 10:51:14 -0400

    add heroku host

--- url_tag_builder/settings.py
+++ url_tag_builder/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 MAIN_HOST = 'url-tag-builder.up.railway.app'
-ALLOWED_HOSTS = [MAIN_HOST]
+ALLOWED_HOSTS = [MAIN_HOST, 'https://url-tag-builder.up.railway.app']
 
 
 # Application definition
